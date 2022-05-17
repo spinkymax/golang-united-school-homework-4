a47b8b07bc9c060cf591a94eec41ba299e735bf6
spinkymax <99490816+spinkymax@users.noreply.github.com>
2022-05-17 11:05:15 +0300

    Update string_sum.go

--- string_sum.go
+++ string_sum.go
@@ -1,8 +1,11 @@
 package string_sum
 
 import (
-	"strconv"
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
@@ -11,6 +14,7 @@ var (
 	errorEmptyInput = errors.New("input is empty")
 	// Use when the expression has number of operands not equal to two
 	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
+	errorIncorrectInput = errors.New("incorrect input")
 )
 
 // Implement a function that computes the sum of two int numbers written as a string
@@ -24,19 +28,35 @@ var (
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
-// if input is empty or whitespace
+// if input is empty, has number of operands not equal to two or equals "+" and "-"
+
 	if input == "" || input == " " {
-		return "", errorEmptyInput
+		return "", fmt.Errorf("%w", errorEmptyInput)
 	}
-	//solution with two numbers
-	a, err := strconv.Atoi("input")
-	if err != nil {
-		return "", err
+	if len(input) != 2 {
+		return "", fmt.Errorf("%w", errorNotTwoOperands)
 	}
-	b, err := strconv.Atoi("output")
-	if err != nil {
-		return "", err
+	if input == "+" || input == "-" {
+		return "", fmt.Errorf("%w", errorIncorrectInput)
 	}
-	return strconv.Itoa(a + b), nil
-	return "", nil
+	input = strings.ReplaceAll(input, " ", "")
+
+	return input, err
+
 }
+
+func MyStringSum(input, input1 string) (output string, err error) {
+	//summation
+	x, err := strconv.Atoi(input)
+	if err != nil {
+		return "", err
+	}
+	y, err := strconv.Atoi(input1)
+	if err != nil {
+		return "", err
+	}
+
+	return strconv.Itoa(x + y), nil
+
+}
+
