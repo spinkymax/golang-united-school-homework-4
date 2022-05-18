d6491bace977b276b8e3e6ef4755b5a85da6bbfb
spinkymax <99490816+spinkymax@users.noreply.github.com>
2022-05-18 23:36:05 +0300

    Update string_sum.go

--- string_sum.go
+++ string_sum.go
@@ -8,34 +8,23 @@ import (
 )
 
 
-//use these errors as appropriate, wrapping them with fmt.Errorf function
 var (
 	// Use when the input is empty, and input is considered empty if the string contains only whitespace
 	errorEmptyInput = errors.New("input is empty")
 	// Use when the expression has number of operands not equal to two
 	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
-	)
 
-// Implement a function that computes the sum of two int numbers written as a string
-// For example, having an input string "3+5", it should return output string "8" and nil error
-// Consider cases, when operands are negative ("-3+5" or "-3-5") and when input string contains whitespace (" 3 + 5 ")
-//
-//For the cases, when the input expression is not valid(contains characters, that are not numbers, +, - or whitespace)
-// the function should return an empty string and an appropriate error from strconv package wrapped into your own error
-// with fmt.Errorf function
-//
-// Use the errors defined above as described, again wrapping into fmt.Errorf
+	errorIncorrectInput = errors.New("incorrect input")
+)
 
 func StringSum(input string) (output string, err error) {
-     // if input is empty, has number of operands not equal to two or equals "+" and "-"
+	// if input is empty, has number of operands not equal to two or equals "+" and "-"
 
 	if input == "" || input == " " {
 		return "", fmt.Errorf("%w", errorEmptyInput)
 	}
 
 	input = strings.TrimSpace(input)
-	input = strings.ReplaceAll(input, "+", "")
-	input = strings.ReplaceAll(input, "-", "")
 
 	var input1 = strings.Fields(input)
 	if len(input1) != 2 {
@@ -54,5 +43,28 @@ func StringSum(input string) (output string, err error) {
 		return "", fmt.Errorf("the error at the second operand:%w", myerr2)
 	}
 	output = strconv.Itoa(x + y)
+
+	//if input has characters
+
+	for _, a := range input {
+		if !strings.Contains("0123456789+- ", string(a)) {
+			return "", fmt.Errorf("sorry, but: %w", errorIncorrectInput)
+		}
+	}
+
 	return output, nil
+}
+
+func NegNums(input string) []string {
+	result := make([]string, 0)
+
+	len := ""
+	for _, i := range input {
+		if string(input[i]) == "-" || string(input[i]) == "+" {
+			len = len + "." + string(input[i])
+		} else {
+			len = len + string(input[i])
+		}
+	}
+	return result
 }
